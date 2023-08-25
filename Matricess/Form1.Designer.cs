f1d485b70860a7993b487404076d60e5ec066868
Pablo Exequiel Gutierrez <pablo.e.gutierrez@outlook.com>
2023-08-25 01:38:44 -0300

    Agregar archivos de proyecto.

---
+++ Matricess/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Matricess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btListar = new System.Windows.Forms.Button();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btListar
+            // 
+            this.btListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btListar.Location = new System.Drawing.Point(12, 56);
+            this.btListar.Name = "btListar";
+            this.btListar.Size = new System.Drawing.Size(163, 44);
+            this.btListar.TabIndex = 0;
+            this.btListar.Text = "Listar";
+            this.btListar.UseVisualStyleBackColor = true;
+            this.btListar.Click += new System.EventHandler(this.btListar_Click);
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalida.Location = new System.Drawing.Point(181, 50);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(0, 31);
+            this.lblSalida.TabIndex = 1;
+            // 
+            // txtDia
+            // 
+            this.txtDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDia.Location = new System.Drawing.Point(12, 12);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(245, 38);
+            this.txtDia.TabIndex = 2;
+            // 
+            // btCargar
+            // 
+            this.btCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCargar.Location = new System.Drawing.Point(263, 8);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(163, 44);
+            this.btCargar.TabIndex = 3;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.btCargar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.btListar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btListar;
+        private System.Windows.Forms.Label lblSalida;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Button btCargar;
+    }
+}
+
---
+++ Matricess/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matricess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] DiasSemana = new string[7];
+        int i = 0;
+        private void btListar_Click(object sender, EventArgs e)
+        {
+            //string [] DiasSemana = new string[7];
+            // string[] DiaSemana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+            //string[] DiasSemana = new string[7];
+            //DiasSemana[0] = "Lunes";
+            //DiasSemana[1] = "Martes";
+            //DiasSemana[2] = "Miercoles";
+            //DiasSemana[3] = "Jueves";
+            //DiasSemana[4] = "Viernes";
+            //DiasSemana[5] = "Sabado";
+            //DiasSemana[6] = "Domingo";
+
+            lblSalida.Text = "";
+            foreach (string dia in DiasSemana)
+            {
+                lblSalida.Text = lblSalida.Text + dia  + "\r\n";
+
+
+            }
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+            DiasSemana[i] = txtDia.Text;
+
+            i = i + 1;
+        }
+    }
+}
